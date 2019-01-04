82420f3f4c1fbe020442f769f3effebe22acf987
DenilsonDesigns <daniellanguilller@gmail.com>
2019-01-04 22:00:06 +1100

    added navbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Wrapper from "./../../hoc/Wrapper";
 import classes from "./Layout.module.css";
+import Toolbar from "./../Navigation/Toolbar/Toolbar";
 
 const Layout = props => {
   return (
     <Wrapper>
-      <div>Toolbar, SideDrawer, Backdrop</div>
+      <Toolbar />
       <main className={classes.Content}>{props.children}</main>
     </Wrapper>
   );

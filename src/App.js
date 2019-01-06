fa86ec51b2f7eeed3e887a3d351f2344cbd3ec09
DenilsonDesigns <daniellanguilller@gmail.com>
2019-01-06 20:45:59 +1100

    start routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Layout from "./hoc/Layout/Layout";
+import Checkout from "./containers/Checkout/Checkout";
+import { Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
+          <Route exact path="/" component={BurgerBuilder} />
+          <Route path="/checkout" component={Checkout} />
         </Layout>
       </div>
     );
